ba3f24609e6b1a4a92fe6b496266fae9281d87a0
Prince1337 <prince.pieritz@gmail.com>
2022-01-08 21:49:47 +0100

    refactoring

--- src/RussischeBauernmultiplikation.java
+++ src/RussischeBauernmultiplikation.java
@@ -1,2 +1,27 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class RussischeBauernmultiplikation {
+
+  public int multiplikation(int erstesProdukt, int zweitesProdukt){
+    Map<Integer, Integer> addition = new HashMap<>();
+
+    while (erstesProdukt != 0){
+      if(erstesProdukt % 2 != 0) {
+        addition.put(erstesProdukt, zweitesProdukt);
+      }
+      erstesProdukt /= 2;
+      zweitesProdukt *= 2;
+    }
+
+    return getSumme(addition);
+  }
+
+  private int getSumme(Map<Integer, Integer> addition) {
+    int count = 0;
+    for (int number : addition.values()){
+      count += number;
+    }
+    return count;
+  }
 }
--- src/RussischeBauernmultiplikationTest.java
+++ src/RussischeBauernmultiplikationTest.java
@@ -1,5 +1,36 @@
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class RussischeBauernmultiplikationTest {
+
+  RussischeBauernmultiplikation russischeBauernmultiplikation;
+
+  @Test
+  void mul47And42(){
+    russischeBauernmultiplikation = new RussischeBauernmultiplikation();
+
+    int actual = russischeBauernmultiplikation.multiplikation(47, 42);
+
+    assertEquals(1974, actual);
+  }
+
+  @Test
+  void mul47And0(){
+    russischeBauernmultiplikation = new RussischeBauernmultiplikation();
+
+    int actual = russischeBauernmultiplikation.multiplikation(47, 0);
+
+    assertEquals(0, actual);
+  }
+
+  @Test
+  void mul12And12(){
+    russischeBauernmultiplikation = new RussischeBauernmultiplikation();
+
+    int actual = russischeBauernmultiplikation.multiplikation(12, 12);
+
+    assertEquals(144, actual);
+  }
 
 }
